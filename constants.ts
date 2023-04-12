42cf2503277db23de96edf99292def7c01408bc8
MariaMagallon <maria.magallon@estudiantat.upc.edu>
2023-04-12 22:35:14 +0200

    mov exmpl to folder+create my bun proj+ApiKeyConst

---
+++ constants.ts
@@ -0,0 +1,1 @@
+export const apiKey: string = "ed0d477b1da570a6def2141ead755887";
--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-import { writeFile } from "fs/promises";
-import { render } from "./render.js";
-import { loadUsers } from "./users.js";
-
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
-
+console.log("Hello via Bun!");
