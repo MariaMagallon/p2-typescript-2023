00be620bdd8c30823074c0bae7508c5b5cfd0e40
MariaMagallon <maria.magallon@estudiantat.upc.edu>
2023-04-16 20:43:35 +0200

    ERROR CODE: fail pagination aproach

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import { writeFile } from "fs/promises";
 import { render } from "./render.js";
 import { loadFilms } from "./films.js";
-
-const films = await loadFilms(0);
+import { apiUrl } from "./constants.js";
+const films = await loadFilms(apiUrl);
 const html = render(films);
 await writeFile('films.html', await html);
+
+//intento de pagination fallido porque no tengo acceso a elementos que aun no estan creados con html por lo que no puedo adjudicar eventos nuevos
